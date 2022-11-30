ad3989c99f88ce5470bab213fc8a1a9358041984
santiagohch <80603960+shchUNAL@users.noreply.github.com>
2022-11-30 15:15:34 -0500

    Mejora filtros
    Se añade funcionalidad para busquedas de hasta 3 tipos de filtros en simultaneo

--- ConectArte/apps/vacantes/views.py
+++ ConectArte/apps/vacantes/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView, View
 from django.shortcuts import redirect, render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.db.models import Q
 from apps.Usuarios.models import Usuario
 from .forms import *
 from apps.DM.models import *
@@ -21,6 +22,9 @@ class VacanteView(LoginRequiredMixin, View):
         animador_cb = request.GET.get('animador_cb')
         pintor_cb = request.GET.get('pintor_cb')
         artesano_cb = request.GET.get('artesano_cb')
+        fotografo_cb = request.GET.get('fotografo_cb')
+        escritor_cb = request.GET.get('escritor_cb')
+        
         max_num = request.GET.get('filtro_maximo')
         min_num = request.GET.get('filtro_minimo')
 
@@ -30,7 +34,207 @@ class VacanteView(LoginRequiredMixin, View):
         if is_valid_param(max_num):
             Vacantes = Vacantes.filter(Pago__lt=max_num)
 
-        if cantante_cb == 'on':
+        if cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+        
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=7))
+        
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+            Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6))
+        
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and animador_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)) 
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4)) 
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=5))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=6))
+
+        elif cantante_cb == 'on' and musico_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4))
+            
+        elif cantante_cb == 'on' and animador_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=5))
+
+        elif cantante_cb == 'on' and animador_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=6))
+
+        elif cantante_cb == 'on' and animador_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5))
+            
+        elif cantante_cb == 'on' and pintor_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6))
+
+        elif cantante_cb == 'on' and pintor_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+
+        elif cantante_cb == 'on' and artesano_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and animador_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)) 
+
+        elif musico_cb == 'on' and animador_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=5)) 
+
+        elif musico_cb == 'on' and animador_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=6))
+
+        elif musico_cb == 'on' and animador_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5))
+
+        elif musico_cb == 'on' and pintor_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6))
+            
+        elif musico_cb == 'on' and pintor_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+
+        elif musico_cb == 'on' and artesano_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif animador_cb == 'on' and pintor_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5))
+            
+        elif animador_cb == 'on' and pintor_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6))
+
+        elif animador_cb == 'on' and pintor_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=7))
+
+        elif animador_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+
+        elif animador_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif pintor_cb == 'on' and artesano_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+
+        elif pintor_cb == 'on' and artesano_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=7))
+
+        elif pintor_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif artesano_cb == 'on' and fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        elif cantante_cb == 'on' and musico_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=2))
+        
+        elif cantante_cb == 'on' and animador_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=3))
+        
+        elif cantante_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=4))
+
+        elif cantante_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=5))
+        
+        elif cantante_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=6))
+        
+        elif cantante_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=1)|Q(CategoriaVacante_id=7))
+
+        elif musico_cb == 'on' and animador_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=3))
+
+        elif musico_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=4))
+
+        elif musico_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=5))
+
+        elif musico_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=6))
+
+        elif musico_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=2)|Q(CategoriaVacante_id=7))
+
+        elif animador_cb == 'on' and pintor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=4))
+
+        elif animador_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=5))
+
+        elif animador_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=6))
+
+        elif animador_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=3)|Q(CategoriaVacante_id=7))
+
+        elif pintor_cb == 'on' and artesano_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=5))
+
+        elif pintor_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=6))
+
+        elif pintor_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=4)|Q(CategoriaVacante_id=7))
+
+        elif artesano_cb == 'on' and fotografo_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=6))
+
+        elif artesano_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=5)|Q(CategoriaVacante_id=7))
+
+        elif fotografo_cb == 'on' and escritor_cb == 'on':
+                    Vacantes = Vacantes.filter(Q(CategoriaVacante_id=6)|Q(CategoriaVacante_id=7))
+
+        
+
+        elif cantante_cb == 'on':
             Vacantes = Vacantes.filter(CategoriaVacante_id=1)
 
         elif musico_cb == 'on':
@@ -44,7 +248,14 @@ class VacanteView(LoginRequiredMixin, View):
 
         elif artesano_cb == 'on':
             Vacantes = Vacantes.filter(CategoriaVacante_id=5)
-            
+
+        elif fotografo_cb == 'on':
+            Vacantes = Vacantes.filter(CategoriaVacante_id=6)
+
+        elif escritor_cb == 'on':
+            Vacantes = Vacantes.filter(CategoriaVacante_id=7)
+        
+
         
         context={
                 'vacantes': Vacantes,
